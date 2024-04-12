f518cab209bbd8b841b74c73cf59ef36ae3e5602
CarlosLopez <carlosgablopezm@gmail.com>
2024-04-12 11:58:45 -0500

    Enrutamiento a pantallas

--- app/src/main/java/com/example/taller2/MainActivity.kt
+++ app/src/main/java/com/example/taller2/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.taller2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.taller2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imagebutton.setOnClickListener{
+            startActivity(Intent(this,ContactosActivity::class.java))
+        }
+        binding.imagebutton2.setOnClickListener{
+            startActivity(Intent(this,CamaraActivity::class.java))
+        }
+        binding.imagebutton3.setOnClickListener{
+            startActivity(Intent(this,MapsActivity::class.java))
+        }
+
     }
 }
