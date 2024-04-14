d92cd0fe5ab8955fd6bee33c0f0bf706812fbe97
CarlosLopez <carlosgablopezm@gmail.com>
2024-04-14 16:03:33 -0500

    camara y galeria funcional

--- app/src/main/java/com/example/taller2/CamaraActivity.kt
+++ app/src/main/java/com/example/taller2/CamaraActivity.kt
@@ -1,11 +1,98 @@
 package com.example.taller2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.taller2.databinding.ActivityCamaraBinding
 
 class CamaraActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCamaraBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_camara)
+        binding = ActivityCamaraBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btncamara.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA_PERMISSION)
+            } else {
+                setupImageView()
+            }
+        }
+        binding.btn.setOnClickListener {
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_MEDIA_IMAGES), REQUEST_GALLERY_PERMISSION)
+            } else {
+                openGallery()
+            }
+        }
+    }
+
+    private fun setupImageView() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(this, "Camera app not available", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun openGallery() {
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(galleryIntent, REQUEST_GALLERY_ACCESS)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                REQUEST_IMAGE_CAPTURE -> {
+                    val imageBitmap = data?.extras?.get("data") as Bitmap
+                    binding.imageView.setImageBitmap(imageBitmap)
+                }
+                REQUEST_GALLERY_ACCESS -> {
+                    val selectedImage = data?.data
+                    binding.imageView.setImageURI(selectedImage)
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CAMERA_PERMISSION -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    setupImageView()
+                } else {
+                    Toast.makeText(this, "Camera permission is required", Toast.LENGTH_SHORT).show()
+                }
+            }
+            REQUEST_GALLERY_PERMISSION -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    openGallery()
+                } else {
+                    Toast.makeText(this, "Gallery permission is required", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val REQUEST_IMAGE_CAPTURE = 1
+        const val REQUEST_CAMERA_PERMISSION = 2
+        const val REQUEST_GALLERY_ACCESS = 3
+        const val REQUEST_GALLERY_PERMISSION = 5
     }
 }
