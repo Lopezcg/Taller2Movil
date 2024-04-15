8464772e73a74372550192537989664abb252e46
CarlosLopez <carlosgablopezm@gmail.com>
2024-04-15 15:48:53 -0500

    Mapa ya se muestra

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,13 @@
+import java.util.Properties
+
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localProperties.load(localPropertiesFile.inputStream())
+}
+
+val mapsApiKey: String? = localProperties.getProperty("MAPS_API_KEY")
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -6,7 +16,9 @@ plugins {
 android {
     namespace = "com.example.taller2"
     compileSdk = 34
-
+    defaultConfig {
+        manifestPlaceholders["MAPS_API_KEY"] = mapsApiKey ?: ""
+    }
     defaultConfig {
         applicationId = "com.example.taller2"
         minSdk = 29
@@ -44,7 +56,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.gms:play-services-maps:18.0.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
