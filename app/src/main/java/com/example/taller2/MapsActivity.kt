a2b69f7adeb753c905ed9f214bde22f6ce4b1178
CarlosLopez <carlosgablopezm@gmail.com>
2024-04-15 17:14:36 -0500

    mapa ya con ubicación usuario

--- app/src/main/java/com/example/taller2/MapsActivity.kt
+++ app/src/main/java/com/example/taller2/MapsActivity.kt
@@ -1,29 +1,129 @@
 package com.example.taller2
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.inputmethod.EditorInfo
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.taller2.databinding.ActivityMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
+import java.io.IOException
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
-    private lateinit var mMap: GoogleMap
+
+    private var mMap: GoogleMap? = null
+    private var geocoder: Geocoder? = null
+
+    private lateinit var mSensorManager: SensorManager
+    private lateinit var mLigthSensor: Sensor
+    private lateinit var mLigthSensorEventListener: SensorEventListener
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var binding: ActivityMapsBinding
+
+
+    override fun onResume() {
+        super.onResume()
+        mSensorManager.registerListener(mLigthSensorEventListener, mLigthSensor, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+    override fun onPause() {
+        super.onPause()
+        mSensorManager.unregisterListener(mLigthSensorEventListener)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        mLigthSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)!!
+
+        mLigthSensorEventListener = object : SensorEventListener{
+            override fun onSensorChanged(event: SensorEvent?) {
+                if (mMap != null) {
+                    if (event!!.values[0] < 10000) {
+                        Log.i("MAPS", "DARK MAP " + event.values[0])
+                        mMap!!.setMapStyle(MapStyleOptions.loadRawResourceStyle(baseContext, R.raw.map_style_dark))
+                    } else {
+                        Log.i("MAPS", "LIGHT MAP " + event.values[0])
+                        mMap!!.setMapStyle(MapStyleOptions.loadRawResourceStyle(baseContext, R.raw.map_style_retro))
+                    }
+                }
+
+            }
+
+            override fun onAccuracyChanged(p0: Sensor?, p1: Int) {}
+
+
+        }
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        binding.texto.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_SEND) {
+                val texto = binding.texto.text.toString()
+                if (texto.isNotEmpty()) {
+                    try {
+                        if (mMap != null && geocoder != null) {
+                            val addresses = geocoder!!.getFromLocationName(texto, 2)
+                            if (addresses != null && addresses.isNotEmpty()) {
+                                val addressResult = addresses[0]
+                                val position =
+                                    LatLng(addressResult.latitude, addressResult.longitude)
+
+                                //Agregar Marcador al mapa
+                                mMap!!.moveCamera(CameraUpdateFactory.zoomTo(15F))
+                                mMap!!.moveCamera(CameraUpdateFactory.newLatLng(position))
+                                mMap!!.addMarker(
+                                    MarkerOptions().position(position)
+                                        .title("Posicion Geocoder")
+                                        .snippet("algo 1")
+                                        .alpha(1f)
+                                )
+                            } else {
+                                Toast.makeText(this, "Dirección no encontrada", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        }
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+
+                } else {
+                    Toast.makeText(this, "La dirección esta vacía", Toast.LENGTH_SHORT).show()
+
+                }
+
+            }
+            true
+        }
+
     }
 
     /**
@@ -37,10 +137,89 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        geocoder = Geocoder(baseContext)
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap!!.uiSettings.isZoomGesturesEnabled = true
+        mMap!!.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.map_style_dark))
+        setupMap()
+
+        // Add a marker in Plaza Bolivar and move the camera
+        val mk1 = LatLng(4.59580, -74.06578)
+        val mk2 = LatLng(4.59580, -74.10590)
+        val mk3 = LatLng(4.60580, -74.06590)
+        mMap!!.moveCamera(CameraUpdateFactory.zoomTo(15F))
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(mk1))
+
+
+
     }
+    private fun setupMap() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+            return
+        }
+
+        // Enable the my location layer on the map
+        mMap?.isMyLocationEnabled = true
+
+        // Get the last known location of the device
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            if (location != null) {
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                mMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
+            } else {
+                Toast.makeText(this, "Location not found", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
+
+    private fun bitmapDescriptorFromVector(context: Context, vectorResId: Int): BitmapDescriptor {
+        val vectorDrawable: Drawable? = ContextCompat.getDrawable(context, vectorResId)
+        vectorDrawable?.setBounds(0,
+            0,
+            vectorDrawable.intrinsicWidth,
+            vectorDrawable.intrinsicHeight)
+        val bitmap = Bitmap.createBitmap(
+            vectorDrawable!!.intrinsicWidth,
+            vectorDrawable.intrinsicHeight,
+            Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        vectorDrawable.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission was granted. Do the contacts-related task you need to do.
+                setupMap()
+            } else {
+                // Permission denied, Disable the functionality that depends on this permission.
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+
 }
